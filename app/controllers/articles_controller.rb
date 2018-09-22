9bd6ef6f19f6e31e84032bcdd06bf0050d6c58a9
aesthetism <zman97619@gmail.com>
2018-09-22 14:20:16 +0530

    articleslisting.work

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,23 @@
 class ArticlesController < ApplicationController
+    def index
+        @article = Article.all
+    end
+
     def new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :text))
+        @article.save
+        redirect_to @article
     end
     def show
         @article = Article.find(params[:id])
     end
-    def create
-        @article = Article.new(params.require(:article).permit(:title, :text))
-      @article.save
-    redirect_to @article
-    end
-
+    
     private
-        def article_params
+    def article_params
         params.require(:article).permit(:title, :text)
-        end
+    end
 
 end
