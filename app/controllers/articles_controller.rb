4877888efe4ddd126f67dd3eba589a2df65a613d
aesthetism <zman97619@gmail.com>
2018-09-22 13:24:21 +0530

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,18 @@
+class ArticlesController < ApplicationController
+    def new
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :text))
+      @article.save
+    redirect_to @article
+    end
+
+    private
+        def article_params
+        params.require(:article).permit(:title, :text)
+        end
+
+end
